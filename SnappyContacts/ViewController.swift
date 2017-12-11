364537c655853757abbfe6f6cfcd324554291749
Dari Curry <dari@dari-harts-imac.local>
2017-12-11 09:04:20 -0600

    Initial Commit

---
+++ SnappyContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnappyContacts
+//
+//  Created by Dari Hart on 12/11/17.
+//  Copyright © 2017 Michael G. Wallin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
